f6f50f28d32af464dc83c4e0b3b3c5507b9d7c9d
v-a-l-e-r-i <valeria.dyvak@gmail.com>
2025-12-21 17:18:23 +0200

    design main player

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from pygame import *
 
+# pip install pygame-ce
+
 init()
 window_size = 500, 400
 window = display.set_mode(window_size)
 clock = time.Clock()
 
-rect = Rect(0, 0, 50, 50)
+class Ball:
+    def __init__(self, x, y, radius, color=None):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
 
-little_leaf = image.load('assets_level/little_leaf.png')
-little_leaf = transform.scale(little_leaf, (50, 50))
+    def reset(self):
+        draw.circle(window, self.color, (self.x, self.y), self.radius)
+
+b1 = Ball(0, 0, 25, (255, 0, 0)) #(0, 0, 25, (255, 0, 0))
 
 while True:
     window.fill((0,0,0))
@@ -17,19 +26,16 @@ while True:
         if e.type == QUIT:
             quit()
 
-    draw.rect(window, (255, 0, 0), rect)
+    b1.reset()
 
-    window.blit(little_leaf, (100, 200))
-
-    
     if keys[K_w]:
-        rect.y -= 5
+        b1.y -= 5
     if keys[K_s]:
-        rect.y += 5
+        b1.y += 5
     if keys[K_d]:
-        rect.x += 5
+        b1.x += 5
     if keys[K_a]:
-        rect.x -= 5
+        b1.x -= 5
 
     display.update()
     clock.tick(60)
